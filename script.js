397b5c8e77e5992bc5ba0de50d0d5cd1ec3103b0
Paul Stevans <pts5di@gmail.com>
2020-12-19 21:47:52 -0800

    Add functionality for checking boxes and adding new items

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
-window.checklistItems = ["Make lunch", "Put lunch in bag", "Bring bag to work"];
+window.checklistItems = [
+  {name:"Make lunch", isChecked:true}, 
+  {name:"Put lunch in bag", isChecked:false}, 
+  {name:"Bring lunch to work", isChecked:false}
+];
 
-function appendChecklistItemHTML(itemText, checklist) {
+
+function onCreateItemClicked(event){
+  	let textEntry = document.getElementById("newItem");
+  	event.preventDefault();
+  	addItem(textEntry.value);
+    textEntry.value = "";
+}
+
+function onItemCheckboxClicked(event){
+    let checkbox = event.target;
+    let position = Number.parseInt(checkbox.id);
+    checklistItems[position].isChecked = checkbox.checked;
+}
+
+document.getElementById("submit").addEventListener("click", onCreateItemClicked);
+
+
+function appendChecklistItemHTML(item, position, checklist) {
   let checkboxElement = document.createElement("input");
   checkboxElement.setAttribute("type", "checkbox");
+  checkboxElement.id = position;
+  checkboxElement.checked = item.isChecked;
+  checkboxElement.addEventListener("click", onItemCheckboxClicked);
   let labelElement = document.createElement("label");
-  labelElement.innerHTML= itemText;
+  labelElement.innerHTML = item.name;
+  labelElement.htmlFor = position;
   let breakElement = document.createElement("br");
   checklist.appendChild(checkboxElement);
   checklist.appendChild(labelElement);
@@ -16,12 +41,12 @@ function repopulate(){
 	let checklist = document.getElementById("checklist");
   checklist.innerHTML = "<legend>My Tasks</legend>";
   for(let i=0; i<checklistItems.length; i++) {
-  	appendChecklistItemHTML(checklistItems[i], checklist);
+  	appendChecklistItemHTML(checklistItems[i], i, checklist);
   }
 }
 
 function addItem(itemText) {
-	checklistItems.push(itemText);
+	checklistItems.push({name:itemText, isChecked:false});
   repopulate();
 }
 repopulate();
