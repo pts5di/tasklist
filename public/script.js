b085083483df6e47614be1c437be7eecb288ac7a
Paul Stevans <pts5di@gmail.com>
2020-12-26 18:38:38 -0800

    Integrate MongoDB + API server with front end

--- public/script.js
+++ public/script.js
@@ -1,21 +1,40 @@
-window.checklistItems = [
-  {name:"Make lunch", isChecked:true}, 
-  {name:"Put lunch in bag", isChecked:false}, 
-  {name:"Bring lunch to work", isChecked:false}
-];
+const SERVER_URL = "http://52.250.52.230:3000";
 
-
-function onCreateItemClicked(event){
-  	let textEntry = document.getElementById("newItem");
-  	event.preventDefault();
-  	addItem(textEntry.value);
-    textEntry.value = "";
+function initialize() {
+  var queryParams = new URLSearchParams(window.location.search);
+  if (!queryParams.has("tasklistId")) {
+    fetch(`${SERVER_URL}/create`, { method: "POST" }).then(response => response.json()).then(json => {
+      let tasklistId = json._id;
+      let url = new URL(window.location.href);
+      let queryParams = new URLSearchParams(url.search);
+      queryParams.append("tasklistId", tasklistId);
+      url.search = queryParams;
+      window.location.href = url;
+    });
+  } else {
+    let tasklistId = queryParams.get("tasklistId");
+    repopulate(fetch(`${SERVER_URL}/tasklist/${tasklistId}`, { method: "GET" }));
+  }
 }
 
-function onItemCheckboxClicked(event){
-    let checkbox = event.target;
-    let position = Number.parseInt(checkbox.id);
-    checklistItems[position].isChecked = checkbox.checked;
+function onCreateItemClicked(event) {
+  let textEntry = document.getElementById("newItem");
+  event.preventDefault();
+  addItem(textEntry.value);
+  textEntry.value = "";
+}
+
+function onItemCheckboxClicked(event) {
+  let checkbox = event.target;
+  let position = Number.parseInt(checkbox.id);
+  let task = window.checklistItems[position];
+  task.isChecked = checkbox.checked;
+  repopulate(fetch(`${SERVER_URL}/tasklist/${window.tasklistId}/${task._id}`, {
+    method: "POST", headers: {
+      "content-type": "application/json"
+    },
+    body: JSON.stringify(task)
+  }));
 }
 
 document.getElementById("submit").addEventListener("click", onCreateItemClicked);
@@ -37,16 +56,21 @@ function appendChecklistItemHTML(item, position, checklist) {
 }
 
 
-function repopulate(){
-	let checklist = document.getElementById("checklist");
-  checklist.innerHTML = "<legend>My Tasks</legend>";
-  for(let i=0; i<checklistItems.length; i++) {
-  	appendChecklistItemHTML(checklistItems[i], i, checklist);
-  }
+function repopulate(fetchPromise) {
+  fetchPromise.then(response => response.json()).then(json => {
+    window.tasklistId = json._id;
+    window.checklistItems = json.tasks;
+    let checklist = document.getElementById("checklist");
+    checklist.innerHTML = "<legend>My Tasks</legend>";
+    for (let i = 0; i < checklistItems.length; i++) {
+      appendChecklistItemHTML(checklistItems[i], i, checklist);
+
+    }
+  })
 }
 
 function addItem(itemText) {
-	checklistItems.push({name:itemText, isChecked:false});
-  repopulate();
+  repopulate(fetch(`${SERVER_URL}/tasklist/${window.tasklistId}/add`, { method: "POST" }));
 }
-repopulate();
+
+initialize();
